59fead22540eff742faa7d3640a31677a96605db
celagio2026 <117282010+celagio2026@users.noreply.github.com>
2023-02-12 11:34:47 +0800

    Add files via upload

---
+++ end.js
@@ -0,0 +1,9 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+finalScore.innerText = mostRecentScore;
+
+
+
